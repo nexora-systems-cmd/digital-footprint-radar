c0e6d723abeac2e899eae08a8791545a7a5eab8d
nexora.systems <greyvector.lab@gmail.com>
2026-09-20 22:43:25 +0300

    Add user and architecture information

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import socket
 import platform
+import getpass
 
 hostname = socket.gethostname()
 system = platform.system()
 kernel = platform.release()
+username = getpass.getuser()
+architecture = platform.machine()
 
 print("=== Digital Footprint Radar v0.1 ===")
 print("Hostname:", hostname)
 print("System:", system)
 print("Kernel:", kernel)
+print("User:", username)
+print("Architecture:", architecture)
